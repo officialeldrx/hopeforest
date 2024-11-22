a9f84300a66475a1e33865ef06465ba3cf519d4a
Forrest Weigel <eldrxofficial@gmail.com>
2024-11-21 20:02:55 -0600

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { FullWidthVideoComponent } from "@/components/full-width-video"
-import ContentSection from "@/components/ui/content-section"
+import ContentSection, { section } from "@/components/ui/content-section"
 import Image from "next/image"
 
 const imageArray = [
@@ -8,15 +8,25 @@ const imageArray = [
     { image: '/images/house.jpg', icon: '/icons/share_white.png', label: 'share' },
 ]
 
+const sections: section[] = [
+    {
+        title: "What Is Hope Forest?",
+        body: "Hope Forest is a place to grow hope in Christ.  We share the hope of Christ in outreach camps and events, help people renew hope in Christ when they are hurting, and encourage people to share the hope of Christ by equipping them through outreach program training."
+    },
+    {
+        title: "What We Do"
+    }
+]
+
 export default function Page() {
     return (
         <div className="w-full flex flex-col gap-16 pb-8">
             <FullWidthVideoComponent src="videos/forest.mp4" minHeight="500px" />
 
-            <ContentSection title="What Is Hope Forest?" body="Hope Forest is a place to grow hope in Christ.  We share the hope of Christ in outreach camps and events, help people renew hope in Christ when they are hurting, and encourage people to share the hope of Christ by equipping them through outreach program training." />
+            <ContentSection section={sections[0]} />
 
             <div className="flex flex-col gap-4">
-                <ContentSection title="What We Do" />
+                <ContentSection section={sections[1]} />
                 <div className="w-full flex lg:flex-row flex-col">
                     {imageArray.map((image, index) => (
                         <a key={index} href={`/${image.label}`} className="h-96 flex flex-grow flex-col gap-2 bg-cover bg-center items-center justify-center" style={{ backgroundImage: `url(${image.image})` }}>
